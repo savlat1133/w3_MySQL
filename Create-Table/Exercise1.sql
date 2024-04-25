0fce4e4eeeca673425711513eb73da17fa4cc5a3
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-25 18:02:02 +0100

    Create Exercise1.sql

---
+++ Create-Table/Exercise1.sql
@@ -0,0 +1,7 @@
+-- 1. Write a MySQL query to create a simple table countries including columns country_id, country_name and region_id.
+
+CREATE TABLE countries(
+country_id varchar(2),
+country_name varchar(40),
+region_id decimal(10,0)
+);
